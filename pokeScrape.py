f2e2597ec8499bd771c83372006054b4f8fbdcf0
getusername67 <61632919+getusername67@users.noreply.github.com>
2022-11-23 23:35:38 -0500

    comments
    added comments

--- pokeScrape.py
+++ pokeScrape.py
@@ -26,7 +26,8 @@ driver.get(website)
 
 #collect data
 raidBosses = []
-#names
+
+#Raid boss name
 names = driver.find_elements("xpath", "//div[@class='boss-name']")
 i = 0
 for name in names:
@@ -34,13 +35,15 @@ for name in names:
     raidBosses[i][0] = name.text
     i += 1
 
-#types
+#Type of pokemon
+#Knowing the type of pokemon you are fighting will help you choose your team to confront it
 i = 0
 allPokeTypes = driver.find_elements("xpath", "//div[@class='type-icons']")
 for x in allPokeTypes:
     indivTypes = x.find_elements("xpath", "img")
     k = 0
     for y in indivTypes:
+        #strips the image src to get the type
         deleteSubString1 = y.get_attribute("src").replace("https://assets.thesilphroad.com/img/pogo-assets/type-", "")
         deleteSubString2 = deleteSubString1.replace(".png", "")
 
@@ -50,9 +53,12 @@ for x in allPokeTypes:
         raidBosses[i][2] = "none"
     i += 1
 
-print(raidBosses)
-#difficulties
+#Recommended group size to beat the boss
+#Dark colors indicate a more difficult boss with a certain size group, lighter colors indicate an easier boss with a certain size group
+#The same boss can be impossible to beat on your own, but easy to beat with a group of ten people
+#Find recommended group size by searching through list until the diffculty is medium or easier
 i = 0
+#All web elements that are identified by the class hexagons. This class contains color that identifies how hard it is to beat
 allPokemonDiffs = driver.find_elements("xpath", "//div[@class='hexagons']")
 for x in allPokemonDiffs:
     indivDiffs = x.find_elements("xpath", "div")
