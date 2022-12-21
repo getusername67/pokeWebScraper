bdef62fc2c62d25ab0bd2c725482584239d5bbef
getusername67 <61632919+getusername67@users.noreply.github.com>
2022-12-21 15:15:34 -0500

    email feature
    Added the email feature using Gmail API

--- pokeScrape.py
+++ pokeScrape.py
@@ -1,10 +1,25 @@
 #Matthew Sanders
 #Scrapes raid pokemon data from thesilphroad.com and sends an email with the data
-#Last edited 11/23/22
-
+#Last edited 12/21/22
+from __future__ import print_function
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import base64
+from email.message import EmailMessage
 import time
 from selenium import webdriver
 from selenium.webdriver import Chrome
+
+print("Enter API key: ")
+API_key = input()
+print("Enter client ID key: ")
+clientID = input()
+print("Enter client secret: ")
+clientSecret = input()
 
 """
 difficulty levels and their associated hex colors
@@ -16,6 +31,77 @@ dif4 = easy
 dif5 = very easy
 dif6 = trivial
 """
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://mail.google.com/']
+
+def gmail_send_message(creds, text):
+    """Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        message = EmailMessage()
+
+        message.set_content(text)
+
+        print("Enter email to send to: ")
+        recipient = input()
+        message['To'] = recipient
+        message['From'] = 'gduser2@workspacesamples.dev'
+        message['Subject'] = 'Weekly Raid Bosses'
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+            .decode()
+        
+        create_message = {
+            'raw': encoded_message
+        }
+        # pylint: disable=E1101
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+    return send_message
+
+"""Shows basic usage of the Gmail API.
+Lists the user's Gmail labels.
+"""
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+
+try:
+    # Call the Gmail API
+    service = build('gmail', 'v1', credentials=creds)
+    results = service.users().labels().list(userId='me').execute()
+
+except HttpError as error:
+    # TODO(developer) - Handle errors from gmail API.
+    print(f'An error occurred: {error}')
 
 #create driver
 website = "https://thesilphroad.com/raid-bosses"
@@ -79,7 +165,7 @@ while i < len(raidBosses):
     message += '\n'
     i += 1
 
-print(message)
+gmail_send_message(creds, message)
 
 while True:
     pass
